46e8e83b21b595e06dad98e2a1a523853a10ab22
Heru <heru@wallextech.com>
2018-05-29 22:37:13 +0800

    routingg

---
+++ pages/detail/_id.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Halaman Detail</h1>
+        <p><b>{{ title }}</b></p>
+        <p>{{ body }}</p>
+
+        <br />
+        <button @click="goBack">Kembali</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    function getData() {
+        axios.get('https://jsonplaceholder.typicode.com/posts/'+this.$route.params.id).then(res => {
+            this.body = res.data.body;
+            this.title = res.data.title;
+        });
+    }
+
+    function goBack() {
+        this.$router.go(-1);
+    }
+
+    export default {
+        name: 'list',
+        data() {
+            return {
+                title: "",
+                body: ""
+            }
+        },
+        methods: {
+            getData,
+            goBack
+        },
+        mounted() {
+            this.getData();
+        }
+    }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Halaman List</h1>
+        <table>
+            <thead>
+                <tr>
+                    <td>Judul</td>
+                    <td>Aksi</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(l,key) in list" :key="key">
+                    <td>{{ l.title }}</td>
+                    <td>
+                        <button @click="gotoDetail(l.id)">Detail</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    function getData() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+            this.list = res.data;
+        });
+    }
+
+    function gotoDetail(id) {
+        this.$router.push('detail/'+id);
+    }
+
+    export default {
+        name: 'list',
+        data() {
+            return {
+                list: []
+            }
+        },
+        methods: {
+            getData,
+            gotoDetail
+        },
+        mounted() {
+            this.getData()
+        }
+    }
+</script>
